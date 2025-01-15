6c060bcc1e23e9b52f9d88ef33aae35415bb51a3
Arundhati Pathak <i.am.arupathak@gmail.com>
2025-01-15 16:57:07 +0530

    first code

---
+++ src/main/java/com/compuca/demo/AzureHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.compuca.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compuca/demo/AzureHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compuca.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
