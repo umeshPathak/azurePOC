290c30dfe9404a752e094f8a4ff75582637e01f2
umeshPathak <iamusp@gmail.com>
2025-02-03 17:14:57 +0530

    Update HelloWorldController.java

--- src/main/java/com/compuca/demo/HelloWorldController.java
+++ src/main/java/com/compuca/demo/HelloWorldController.java
@@ -1,13 +1,14 @@
 package com.compuca.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+    public String helloWorld(@RequestParam(required = false, defaultValue = "Guest") String name) {
+        return "Hello World! " + name;
     }
 }
